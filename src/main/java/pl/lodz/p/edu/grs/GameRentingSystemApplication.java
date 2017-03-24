49ba475010c783ba386a5ce4e6cf7957c70063d7
pblazinski <pblazinski12013@gmail.com>
2017-03-24 14:24:44 +0100

    Initialize project structure

---
+++ src/main/java/pl/lodz/p/edu/grs/GameRentingSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz.p.edu.grs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameRentingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameRentingSystemApplication.class, args);
+	}
+}
