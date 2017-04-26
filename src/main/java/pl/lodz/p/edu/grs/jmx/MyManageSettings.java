7ec3993151518c22e55399bb7d1b77d898bfe860
pblazinski <pblazinski12013@gmail.com>
2017-04-26 17:32:00 +0200

    Added jmx and spring actuator

---
+++ src/main/java/pl/lodz/p/edu/grs/jmx/MyManageSettings.java
@@ -0,0 +1,31 @@
+package pl.lodz.p.edu.grs.jmx;
+
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedResource
+public class MyManageSettings  {
+
+    private int maxThreads = -1;
+    private int maxPoolSize = -1;
+
+    @ManagedAttribute(defaultValue = "It's description")
+    public int getMaxPoolSize() {
+        return maxPoolSize;
+    }
+
+    @ManagedAttribute
+    public void setMaxPoolSize(int maxPoolSize) {
+        this.maxPoolSize = maxPoolSize;
+    }
+
+    public int getMaxThreads() {
+        return maxThreads;
+    }
+
+    public void setMaxThreads(int maxThreads) {
+        this.maxThreads = maxThreads;
+    }
+}
