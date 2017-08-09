3f7817d5f164f5f40405bd735301045f2c85beb9
Rafał Koper <rafal14kop@gmail.com>
2017-08-09 20:41:37 +0200

    build fix

--- src/test/java/pl/lodz/p/edu/grs/GameRentingSystemApplicationTests.java
+++ src/test/java/pl/lodz/p/edu/grs/GameRentingSystemApplicationTests.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class GameRentingSystemApplicationTests {
 
+    @Test
 	public void contextLoads() {
 	}
 
